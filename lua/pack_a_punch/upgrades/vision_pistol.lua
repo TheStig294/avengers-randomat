209be2c39f26698e7cb8b304bfbc12ee6e12e32e
thestig294 <thestig294@gmail.com>
2024-07-29 20:40:54 +0930

    Fixed lua error with upgraded Nick Fury pistol

--- lua/pack_a_punch/upgrades/vision_pistol.lua
+++ lua/pack_a_punch/upgrades/vision_pistol.lua
@@ -6,14 +6,22 @@ UPGRADE.desc = "x1.5 firerate, wider FOV"
 UPGRADE.firerateMult = 1.5
 
 function UPGRADE:Apply(SWEP)
-    SWEP:GetOwner():SetFOV(150)
+    local own = self:GetOwner()
+
+    if IsValid(own) then
+        own:SetFOV(150)
+    end
 
     function SWEP:Deploy()
-        SWEP:GetOwner():SetFOV(150)
+        local owner = self:GetOwner()
+        if not IsValid(owner) then return end
+        owner:SetFOV(150)
     end
 
     function SWEP:Holster()
-        SWEP:GetOwner():SetFOV(0)
+        local owner = self:GetOwner()
+        if not IsValid(owner) then return end
+        owner:SetFOV(0)
 
         return true
     end
